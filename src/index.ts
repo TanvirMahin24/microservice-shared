8f1273ec981f177a6d2cdf3201659c9ebb779378
Tanvir Mahin <tanvirmahin24@gmail.com>
2023-06-25 01:09:18 +0600

    Updated

--- src/index.ts
+++ src/index.ts
@@ -18,3 +18,6 @@ export * from "./events/base-publisher";
 export * from "./events/subjects";
 export * from "./events/ticket-created-event";
 export * from "./events/ticket-updated-event";
+
+// TYPES
+export * from "./events/types/order-status";
